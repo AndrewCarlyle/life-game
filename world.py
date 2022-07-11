faa63d8466b027ef2d0e3d6683cd2f9a79db505e
Andrew <andrew.carlyle31@gmail.com>
2022-07-11 18:25:01 -0400

    Adding world file/class

---
+++ world.py
@@ -0,0 +1,31 @@
+from random import randint
+
+X_SIZE = 1000
+Y_SIZE = 1000
+
+#class used to keep track of the players/food using a data structure
+#The data strucutre is a 3D list, which represents a 2D world, where each place
+#in the world has a list of player food (the list for a space can be empty)
+class world:
+    def __init__(self, players, numFood):
+        self.world = []
+
+        #init the 3D list
+        for x in range(X_SIZE):
+            self.world.append([])
+            for y in range(Y_SIZE):
+                self.world[x].append([])
+
+        #Randomly assign player to a place in the world
+        for player in players:
+            x = randint(X_SIZE)
+            y = randint(Y_SIZE)
+
+            self.world[x][y].append([player])
+
+        #Add food to the world
+        for i in range(numFood):
+            x = randint(X_SIZE)
+            y = randint(Y_SIZE)
+
+            self.world[x][y].append("food")
