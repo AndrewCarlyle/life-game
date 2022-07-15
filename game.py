c5c4f814c4add5b763572964b800b07c25eee1d2
Andrew <andrew.carlyle31@gmail.com>
2022-07-14 21:37:33 -0400

    Fix randint calls

--- game.py
+++ game.py
@@ -12,9 +12,7 @@ def main():
     for i in range(NUM_PLAYERS):
         playerList.append(player.player())
 
-    plr = player.player(1,1,1,1,1)
-
-    #wd = world.world(NUM_PLAYERS, NUM_FOOD)
+    wd = world.world(playerList, NUM_FOOD)
 
 if __name__ == "__main__":
     main()
