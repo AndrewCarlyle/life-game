a3c2365c332f36a4620c3d6ad5346a831dbcddca
Andrew <andrew.carlyle31@gmail.com>
2022-07-26 16:56:23 -0400

    Adding very basic end of game steps

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ import random
 #Default settings to start the game
 NUM_PLAYERS = 5
 NUM_FOOD = 5
-NUM_ROUNDS = 5
+NUM_ROUNDS = 20
 
 def main():
     #init players
@@ -16,12 +16,17 @@ def main():
     wd = world.World(playerList, NUM_FOOD)
 
     for i in range(NUM_ROUNDS):
-        wd.print()
+        #wd.print()
         wd.increaseHunger()
         wd.movePlayers()
         wd.checkCollisions()
+        wd.checkDeath()
         wd.addFood(1)
-        print("\nEnd of turn\n")
+        print("\nEnd of turn: ", i, "\n")
+
+    wd.print()
+    print("End of game...")
+    print("Number of players currently alive: ", len(wd.playerList))
 
 if __name__ == "__main__":
     main()
