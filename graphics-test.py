40620bc955d0f8092d29e3d95513d5a6bec61161
Andrew <andrew.carlyle31@gmail.com>
2022-08-31 23:09:58 -0400

    Add in set color commands

--- graphics-test.py
+++ graphics-test.py
@@ -13,9 +13,9 @@ def drawWorld(world):
     for x in range(len(world)):
         for y in range(len(world[x])):
             if len(world[x][y]) > 1:
-                #color green
+                set_color(Color.GREEN)
             elif world[x][y] == ['food']:
-                #color yellow
+                set_color(Color.YELLOW)
             elif len(world[x][y]) == 1:
-                #color red
+                set_color(Color.RED)
             #else color stays white
