ce8fae2e6495d1d636b95669b44a4108b282a7d2
Andrew <andrew.carlyle31@gmail.com>
2022-09-01 17:55:55 -0400

    Moving graphics functions into a class

--- graphics-test.py
+++ graphics-test.py
@@ -1,21 +1,33 @@
 from easygraphics import *
 
-def main():
-    init_graph(1000, 1000)
-    set_color(Color.RED)
-    rect(20,20,39,39)
-    pause()
-    close_graph()
+class draw:
+    def __init__(self):
+        print("test")
+    #def __init__(self, World):
+    #    self.world = World
 
-easy_run(main)
+    def main(self):
+        init_graph(1000, 1000)
+        set_color(Color.RED)
+        rect(20,20,39,39)
+        self.drawWorld(self.world)
+        pause()
+        close_graph()
 
-def drawWorld(world):
-    for x in range(len(world)):
-        for y in range(len(world[x])):
-            if len(world[x][y]) > 1:
-                set_color(Color.GREEN)
-            elif world[x][y] == ['food']:
-                set_color(Color.YELLOW)
-            elif len(world[x][y]) == 1:
-                set_color(Color.RED)
-            #else color stays white
+    def drawWorld(self, world):
+        for x in range(len(world)):
+            for y in range(len(world[x])):
+                #Make this color darker based on how many items are there?
+                if len(world[x][y]) > 1:
+                    set_color(Color.GREEN)
+                elif world[x][y] == ['food']:
+                    set_color(Color.YELLOW)
+                elif len(world[x][y]) == 1:
+                    set_color(Color.RED)
+                #else color stays white
+
+    def refreshImage(self):
+        easy_run(self.main)
+
+d = draw()
+d.refreshImage()
