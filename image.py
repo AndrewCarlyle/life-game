1bc07af175fc3bcd19132d966bfd3ac490b6f85b
Andrew <andrew.carlyle31@gmail.com>
2022-09-04 12:16:16 -0400

    Draw filled in shapes instead of outlines

--- image.py
+++ image.py
@@ -10,14 +10,12 @@ class Image:
             for y in range(len(self.world[x])):
 
                 if all(z == "food" for z in self.world[x][y]) and len(self.world[x][y]) > 0:
-                    set_color(Color.GREEN)
+                    set_fill_color(Color.GREEN)
                 #Make this color darker based on how many items are there?
                 elif len(self.world[x][y]) > 0:
-                    set_color(Color.RED)
-                #elif len(self.world[x][y]) == 1:
-                #    set_color(Color.DARK_YELLOW)
+                    set_fill_color(Color.RED)
                 else:
-                    set_color(Color.WHITE)
+                    set_fill_color(Color.WHITE)
 
                 #May need to change from +10 to +9?
-                rect(x*10,y*10,x*10+9,y*10+9)
+                fill_rect(x*10,y*10,x*10+9,y*10+9)
